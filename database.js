6dbda3ecba6c9c42a235020069e2beed4e62e2f0
Chris Jonathan <christophereko@hotmail.fr>
2021-02-14 13:36:39 +0000

    Migrated DB to MongoDB Atlas and replaced connection string with environment variable.

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // Initiating a connection to the database
-mongoose.connect('mongodb://chris:chris22@ds211774.mlab.com:11774/recipesdatabase', { useNewUrlParser: true }); 
+mongoose.connect(process.env.MONGODB_ATLAS_CONNECTION_STRING, { useNewUrlParser: true }); 
     
 // Connecting to the database
 const connect = mongoose.connection;
