977cac718e1f4da1574cea1750aab098894516e7
Chris Jonathan <chris@chrisjonathan.co.uk>
2023-12-15 18:29:07 +0000

    Updated Mongoose and amended the code to re-establish a connection to the collection

--- database.js
+++ database.js
@@ -1,14 +1,17 @@
 // adding mongoose to this file
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Initiating a connection to the database
+// Make sure that the connection string has the db name after.net/ otherwise a new db called test will be created
+// Therefore you will be looking at the wrong db
 mongoose.connect(process.env.MONGODB_ATLAS_CONNECTION_STRING, { useNewUrlParser: true }); 
     
 // Connecting to the database
 const connect = mongoose.connection;
 
 // Create a Schema or db structure
-let recipeSchema = new mongoose.Schema({
+const recipeSchema = new mongoose.Schema({
     title: String,
     image: String,
     duration: String,
@@ -17,11 +20,10 @@ let recipeSchema = new mongoose.Schema({
 });
 
 // Create a model/ collection
-const recipes = mongoose.model('recipes', recipeSchema);
-
+const Model = mongoose.model("Recipe", recipeSchema);
 
 // Export the connection and the model
 module.exports.connect = connect;
-module.exports.recipes = recipes;
+module.exports.Model = Model;
 
 
